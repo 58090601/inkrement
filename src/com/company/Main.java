d9f63310b51e335516a0d74ac0d2777b75cd158c
Aaaa <Sss@s>
2022-12-19 22:45:40 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        try(Socket socket = new Socket("localhost", 1020)){
+            for(int i = 0; i < 10; i++){
+                OutputStream outputStream = socket.getOutputStream();
+                outputStream.write(i);
+                outputStream.flush();
+
+                InputStream inputStream = socket.getInputStream();
+                int response = inputStream.read();
+                System.out.println("Клиент " +response);
+            }
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
+
+
